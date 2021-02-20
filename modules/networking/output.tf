bb866952bcd05bb03a35399fb9b94b0edf82e886
BaeKY <ksc2601@gmail.com>
2021-02-21 05:45:28 +0900

    Fix networking module output

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -2,12 +2,12 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "public_subnets_id" {
-  value = [aws_subnet.public_subnet.*.id]
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnet.*.id
 }
 
-output "private_subnets_id" {
-  value = [aws_subnet.private_subnet.*.id]
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnet.*.id
 }
 
 output "default_sg_id" {
