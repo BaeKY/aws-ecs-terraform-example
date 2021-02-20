9d4553e002a6667ad014b0325ac528b78297db4c
BaeKY <ksc2601@gmail.com>
2021-02-21 05:44:15 +0900

    Change config

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AWS High Scalability Architecture - Terraform
 
 아직 미완성
+
+```bash
+terraform init -backend-config=YOUR_CONFIG.config
+```
--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "random_id" "random_id_prefix" {
-  byte_length = 2
-}
-/*====
-Variables used across all modules
-======*/
 locals {
   production_availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
 }
